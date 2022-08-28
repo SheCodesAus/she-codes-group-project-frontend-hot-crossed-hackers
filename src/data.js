aa078ab3eee021861605cfa121a3f88303c01bd4
genesisscv <genesisscv@gmail.com>
2022-08-28 20:41:28 +1000

    fixed up syntax in dummy data

--- src/data.js
+++ src/data.js
@@ -4,11 +4,12 @@ export const allScholarships = [
     title: "She Codes - Plus ",
     organisation: "She Codes Australia",
     image: "https://pbs.twimg.com/media/FVNpAhmagAAXkic?format=jpg&name=large",
-    description ,"She Codes Plus is our six month program that is designed to supercharge the tech career of it’s participants – ideal for women looking to enter, or advance in, the tech industry.Plus runs on a part-time basis, outside of business hours, making it perfect for full-time workers, and women returning to the workforce.",
+    description: "She Codes Plus is our six month program that is designed to supercharge the tech career of its participants, ideal for women looking to enter, or advance in, the tech industry.Plus runs on a part-time basis, outside of business hours, making it perfect for full-time workers, and women returning to the workforce.",
     url: "https://shecodes.com.au/program/plus/",
-    closingdate: "2022-11-20T14:22:23.382748Z",
+    closing_date: "2022-11-20T14:22:23.382748Z",
     
-    },
+    }
+    ,
 
     {
     id: 2,
@@ -17,8 +18,9 @@ export const allScholarships = [
     image: "https://www.smartcompany.com.au/wp-content/uploads/2022/01/BHP-768x485-1.jpg",
     description: "The BHP Technical Entry Program (TEP) is a recruitment program offering permanent part-time and full-time job opportunities for new-to-industry talent with relevant experience OR experienced professionals returning to the workforce after a career break or extended absence",
     url: "https://careers.bhp.com/job/Technical-Entry-Pathways/918535700/#:~:text=The%20BHP%20Technical%20Entry%20Program,career%20break%20or%20extended%20absence",
-    closingdate: "2022-12-10T14:22:23.382748Z",
-    },
+    closing_date: "2022-12-10T14:22:23.382748Z",
+    }
+    ,
 
     {
     id: 3,
@@ -27,6 +29,8 @@ export const allScholarships = [
     image: "https://www.informationplanet.com/sites/default/files/styles/infoplanet_original/public/externals/cf28a79ccf6a3e8b0cb9ab29117ad8ef.jpg",
     description :"insert a bunch of info about the scholarship here",
     url: "https://www.coderacademy.edu.au/diversity_in_tech_scholarship/about",
-    closingdate: "2022-09-30T14:22:23.382748Z",
-    },
+    closing_date: "2022-09-30T14:22:23.382748Z",
+    }
+    ,
+
 ]
