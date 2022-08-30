545902cb8c1cabfa23e135d9571f4d598dc28b88
genesisscv <genesisscv@gmail.com>
2022-08-30 19:21:50 +1000

    Dummy data set up!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,28 +2,41 @@ import React, { useState, useEffect } from "react";
 import { allProjects } from "../data";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
-
-// function to get projects from the API below
 function HomePage() {
-    const [projectList, setProjectList] = useState([]);
-
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}projects`)
-        .then((results) => {
-        return results.json();
-        })
-        .then((data) => {
-        setProjectList(data);
-        });
-        },[]);
-
     return (
-        <div id= "project-list">
-        {projectList.map((projectData, key) => {
+        <div id="project-list">
+        {allProjects.map((projectData, key) => {
         return <ProjectCard key={key} projectData={projectData} />;
-        })}
-        </div>
+    })}
+    </div>
     );
 }
+    
+
+
+
+// function to get projects from the API below
+
+// function HomePage() {
+//     const [projectList, setProjectList] = useState([]);
+
+//     useEffect(() => {
+//         fetch(`${process.env.REACT_APP_API_URL}projects`)
+//         .then((results) => {
+//         return results.json();
+//         })
+//         .then((data) => {
+//         setProjectList(data);
+//         });
+//         },[]);
+
+//     return (
+//         <div id= "project-list">
+//         {projectList.map((projectData, key) => {
+//         return <ProjectCard key={key} projectData={projectData} />;
+//         })}
+//         </div>
+//     );
+// }
 
 export default HomePage;
