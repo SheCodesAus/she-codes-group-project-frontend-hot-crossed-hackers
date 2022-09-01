fbd8b1e4bd0bbac5f3fb2d9768046309cbeaeb3c
genesisscv <genesisscv@gmail.com>
2022-09-01 21:51:36 +1000

    created partner cards

---
+++ src/components/PartnerCard/PartnerCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./PartnerCard.css";
+
+function PartnerCard(props) {
+    const { partnerData } = props;
+    return(
+        <div className="partner-card">
+        <Link to={`/partner/${partnerData.id}`}>
+        <img src={partnerData.image}/>
+        <h3>{partnerData.title}</h3>
+        </Link>
+        </div>
+    );
+}
+
+export default PartnerCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { allGrants } from "../data";
 import { allStories } from "../data";
+import { allPartners } from "../data";
 import GrantCard from "../components/GrantCard/GrantCard";
 import StoryCard from "../components/StoryCard/StoryCard";
+import PartnerCard from "../components/PartnerCard/PartnerCard";
 import Hero from "../components/Hero/Hero"
 
 
@@ -11,16 +13,33 @@ function HomePage() {
 
         <div>
             <Hero />
+            <div id="h1-home">
+                <h1>Featured Grants</h1>
+            </div>
             <div id="grant-list">
             {allGrants.map((grantData, key) => {
             return <GrantCard key={key} grantData={grantData} />;
             })}
+            </div>
+
+            <div id="h1-home">
+                <h1>Success Stories</h1>
             </div>
             <div id="story-list">
             {allStories.map((storyData, key) => {
             return <StoryCard key={key} storyData={storyData} />;
             })}
             </div>
+
+            <div id="h1-home">
+                <h1>Our Partners</h1>
+            </div>
+            <div id="partner-list">
+            {allPartners.map((partnerData, key) => {
+            return <PartnerCard key={key} partnerData={partnerData} />;
+            })}
+            </div>
+
 
         </div>
     );
