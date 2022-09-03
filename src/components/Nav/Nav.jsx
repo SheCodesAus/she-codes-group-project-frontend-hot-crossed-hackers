b7a744bfe088d0a70df996753221bb2e3f0c7d84
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-09-03 12:55:38 +1000

    nav updates to develop

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,15 +7,13 @@ function Nav() {
         <nav>
             <div>
                 <Link to="/"><div className="logo" /> <img src={require('./logo.png')} /></Link>
-                {/* <Link to="/"><img className="logo" src="../src/Images/logo.png" alt="logo"></img></Link> */}
-                <Link to="/"><img src={process.env.PUBLIC_URL + '/logo.png'} className="logo" alt="logo"></img></Link>
             </div>
             <ul className="nav.titles">
                 <li>
                     <Link to="grants"> Grants</Link>
                 </li>
                 <li>
-                    <Link to="login" className="loginbtn"> Log in</Link>
+                    <Link to="login"> Log in</Link>
                 </li>
                 <li>
                     <Link to="Signup"> Sign up</Link>
