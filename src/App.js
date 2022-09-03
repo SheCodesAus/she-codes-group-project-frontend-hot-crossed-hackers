5766eb549b9463f31e7d3422cd5321d8dd63475f
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-09-03 14:35:59 +1000

    login/signup styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Nav from "./components/Nav/Nav";
 import HomePage from "./pages/HomePage";
 import GrantsPage from "./pages/GrantsPage";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import AccountPage from "./pages/AccountPage";
 import "./App.css";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route path="grant/:id" element={<GrantsPage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="account" element={<AccountPage />} />
       </Routes>
     </Router>
   );
