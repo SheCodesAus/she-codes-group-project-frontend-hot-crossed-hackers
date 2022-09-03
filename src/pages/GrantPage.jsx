3dd073ff8e7748d703ec7137f63d2fc3f494f917
genesisscv <genesisscv@gmail.com>
2022-09-03 12:56:21 +1000

    working on grants page

--- src/pages/GrantPage.jsx
+++ src/pages/GrantPage.jsx
@@ -1,19 +1,51 @@
+import React from "react";
+import { allGrants } from "../data";
+import { allStories } from "../data";
+import { allPartners } from "../data";
+import GrantCard from "../components/GrantCard/GrantCard";
+import StoryCard from "../components/StoryCard/StoryCard";
+import PartnerCard from "../components/PartnerCard/PartnerCard";
+import Hero from "../components/Hero/Hero"
 
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
 
-function GrantPage() {
+function HomePage() {
+    return (
 
-    const { id } = useParams();
+        <div>
+            <Hero />
+            <div id="h1-home">
+                <h1>Featured Grants</h1>
+            </div>
+            <div id="grant-list">
+            {allGrants.map((grantData, key) => {
+            return <GrantCard key={key} grantData={grantData} />;
+            })}
+            </div>
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}grant/${id}`)
-            .then((results) => {
-                return results.json();
-            })
-            .then((data) => {
-                setGrantData(data);
-            });
-    }, []);
+
+
+        </div>
+    );
+    
 }
-export default GrantPage;
+
+
+// Function to get data from API below
+// import React, { useEffect } from "react";
+// import { useParams } from "react-router-dom";
+
+// function GrantPage() {
+
+//     const { id } = useParams();
+
+//     useEffect(() => {
+//         fetch(`${process.env.REACT_APP_API_URL}grant/${id}`)
+//             .then((results) => {
+//                 return results.json();
+//             })
+//             .then((data) => {
+//                 setGrantData(data);
+//             });
+//     }, []);
+// }
+// export default GrantPage;
