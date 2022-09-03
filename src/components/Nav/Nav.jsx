7c8045ff52a7171fad90f1e314865311efd4d6c5
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-09-03 12:27:04 +1000

    nav logo and positioning updates

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,9 +6,10 @@ function Nav() {
     return (
         <nav>
             <div>
-                <Link to="/"><img className="logo" src="../src/Images/logo.png" alt="logo"></img></Link>
+                <Link to="/"><div className="logo" /> <img src={require('./logo.png')} /></Link>
+                {/* <Link to="/"><img className="logo" src="../src/Images/logo.png" alt="logo"></img></Link> */}
             </div>
-            <ul>
+            <ul className="nav.titles">
                 <li>
                     <Link to="grants"> Grants</Link>
                 </li>
