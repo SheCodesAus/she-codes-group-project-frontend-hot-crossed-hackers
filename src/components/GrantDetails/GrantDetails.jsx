349fe65df12b9039a264dae42d703861e0cc668d
genesisscv <genesisscv@gmail.com>
2022-09-03 22:13:52 +1000

    fixing css

--- src/components/GrantDetails/GrantDetails.jsx
+++ src/components/GrantDetails/GrantDetails.jsx
@@ -9,6 +9,9 @@ function GrantDetails(props) {
         <Link to={`/grant/${grantData.id}`}>
         <img src={grantData.image}/>
         <h3>{grantData.title}</h3>
+        <p>Organisation: {grantData.organisation}</p>
+        <p>Description: {grantData.description}</p>
+        <p>More information at: {grantData.url}</p>
         </Link>
         </div>
     );
