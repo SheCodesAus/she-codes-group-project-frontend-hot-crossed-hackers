979ab9d52c4b021b370bbc1641a0b0910a96df03
genesisscv <genesisscv@gmail.com>
2022-09-03 14:30:31 +1000

    xx

--- src/data.js
+++ src/data.js
@@ -45,29 +45,8 @@ export const oneGrant = [
     url: "https://shecodes.com.au/program/plus/",
     closing_date: "2022-11-20T14:22:23.382748Z",
     
-    }
-    ,
-
-    {
-    id: 2,
-    title: "Technical Entry Pathways",
-    organisation: "BHP",
-    image: "https://www.smartcompany.com.au/wp-content/uploads/2022/01/BHP-768x485-1.jpg",
-    description: "The BHP Technical Entry Program (TEP) is a recruitment program offering permanent part-time and full-time job opportunities for new-to-industry talent with relevant experience OR experienced professionals returning to the workforce after a career break or extended absence",
-    url: "https://careers.bhp.com/job/Technical-Entry-Pathways/918535700/#:~:text=The%20BHP%20Technical%20Entry%20Program,career%20break%20or%20extended%20absence",
-    closing_date: "2022-12-10T14:22:23.382748Z",
-    }
-    ,
-
-    {
-    id: 3,
-    title: "Diversity In Tech Scholarship",
-    organisation: "Coder Academy.",
-    image: "https://www.informationplanet.com/sites/default/files/styles/infoplanet_original/public/externals/cf28a79ccf6a3e8b0cb9ab29117ad8ef.jpg",
-    description :"insert a bunch of info about the scholarship here",
-    url: "https://www.coderacademy.edu.au/diversity_in_tech_scholarship/about",
-    closing_date: "2022-09-30T14:22:23.382748Z",
-    }
+    },
+    
 ]
 
 export const allStories = [
--- src/pages/GrantPage.jsx
+++ src/pages/GrantPage.jsx
@@ -1,33 +1,34 @@
-import React from "react";
-import { oneGrant } from "../data";
-// import GrantCard from "../components/GrantCard/GrantCard";
+// import React from "react";
+// import { oneGrant } from "../data";
+// // import GrantCard from "../components/GrantCard/GrantCard";
 
-
-function GrantPage() {
-    return(
-    <div>
-    <h2>oneGrant.title</h2>
-    </div>
-    )
-}
-export default GrantPage;
-
-// Function to get data from API below
-// import React, { useEffect } from "react";
-// import { useParams } from "react-router-dom";
 
 // function GrantPage() {
-
-//     const { id } = useParams();
-
-//     useEffect(() => {
-//         fetch(`${process.env.REACT_APP_API_URL}grant/${id}`)
-//             .then((results) => {
-//                 return results.json();
-//             })
-//             .then((data) => {
-//                 setGrantData(data);
-//             });
-//     }, []);
+//     return (
+//     <div>
+//     <h2>{oneGrant.title}</h2>
+//     </div>
+//     );
 // }
 // export default GrantPage;
+
+// Function to get data from API below
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { oneGrant } from "../data";
+
+function GrantPage() {
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`${process.oneGrant}grant/${id}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then((data) => {
+                setGrantData(data);
+            });
+    }, []);
+}
+export default GrantPage;
