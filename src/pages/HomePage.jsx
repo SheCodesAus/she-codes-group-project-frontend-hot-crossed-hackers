f652d487484c5bd6b58ebddfb96a05e83c91f342
genesisscv <genesisscv@gmail.com>
2022-09-04 22:34:41 +1000

    connecting api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { allGrants } from "../data";
+// import { allGrants } from "../data"; use this to test dummy data
 import { allStories } from "../data";
 import { allPartners } from "../data";
 import GrantCard from "../components/GrantCard/GrantCard";
@@ -9,39 +9,55 @@ import Hero from "../components/Hero/Hero"
 
 
 function HomePage() {
-    return (
 
-        <div>
-            <Hero />
-            <div id="h1-home">
-                <h1>Featured Grants</h1>
-            </div>
-            <div id="grant-list">
-                {allGrants.map((grantData, key) => {
-                    return <GrantCard key={key} grantData={grantData} />;
+    const [grantList, setGrantList] = useState([]);
+
+        useEffect(() => {
+            fetch(`${process.env.REACT_APP_API_URL}grants`)
+            .then((results) => {
+            return results.json();
+            })
+            .then((data) => {
+            setGrantList(data);
+            });
+            },[]);
+
+        return (
+            <div>
+                <Hero />
+                <div id="h1-home">
+                    <h1>Featured Grants</h1>
+                </div>
+                <div id= "grant-list">
+                {grantList.map((grantData, key) => {
+                return <GrantCard key={key} grantData={grantData} />;
                 })}
-            </div>
+                </div>
+                {/* use function below to test dummy data */}
+                {/* <div id="grant-list">
+                    {allGrants.map((grantData, key) => {
+                        return <GrantCard key={key} grantData={grantData} />;
+                    })}}
+                </div> */}
+                {/* use function above to test dummy data */}
+                <div id="h1-home">
+                    <h1>Success Stories</h1>
+                </div>
+                <div id="story-list">
+                    {allStories.map((storyData, key) => {
+                        return <StoryCard key={key} storyData={storyData} />;
+                    })}
+                </div>
 
-            <div id="h1-home">
-                <h1>Success Stories</h1>
+                <div id="h1-home">
+                    <h1>Our Partners</h1>
+                </div>
+                <div id="partner-list">
+                    {allPartners.map((partnerData, key) => {
+                        return <PartnerCard key={key} partnerData={partnerData} />;
+                    })}
+                </div>
             </div>
-            <div id="story-list">
-                {allStories.map((storyData, key) => {
-                    return <StoryCard key={key} storyData={storyData} />;
-                })}
-            </div>
-
-            <div id="h1-home">
-                <h1>Our Partners</h1>
-            </div>
-            <div id="partner-list">
-                {allPartners.map((partnerData, key) => {
-                    return <PartnerCard key={key} partnerData={partnerData} />;
-                })}
-            </div>
-
-
-        </div>
     );
 
 }
