d8bedcd8ff91346397b58b05b75c63ee6cfac7a0
genesisscv <genesisscv@gmail.com>
2022-09-06 17:42:41 +1000

    using dummy data

--- src/pages/GrantsPage.jsx
+++ src/pages/GrantsPage.jsx
@@ -1,51 +1,53 @@
-// import { useParams } from "react-router-dom"; use this to test dummy data
-// import { allGrants } from "../data"; use this to test dummy data
+import React from "react";
+import { useParams } from "react-router-dom";
+import { allGrants } from "../data"; 
+// use code above for dummy data
 import GrantCard from "../components/GrantCard/GrantCard";
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
 
 
-function GrantPage() {
-    const [grantList, setGrantList] = useState([]);
+// function GrantPage() {
+//     const [grantList, setGrantList] = useState([]);
 
-    useEffect(() => {
-        fetch(`${process.env.REACT_APP_API_URL}grants`)
-        .then((results) => {
-        return results.json();
-        })
-        .then((data) => {
-        setGrantList(data);
-        });
-        },[]);
+//     useEffect(() => {
+//         fetch(`${process.env.REACT_APP_API_URL}grants`)
+//         .then((results) => {
+//         return results.json();
+//         })
+//         .then((data) => {
+//         setGrantList(data);
+//         });
+//         },[]);
 
-    return (
-        <div>
-            <div id= "grant-list">
-            {grantList.map((grantData, key) => {
-            return <GrantCard key={key} grantData={grantData} />;
-            })}
-            </div>
-        </div>
-    );
-}
-export default GrantPage;
-
-// use the function below to test dummy data
-// function GrantsPage() {
 //     return (
-
 //         <div>
-//             <div id="h1-home">
-//                 <h1>Grants</h1>
-//             </div>
-//             <div id="grant-list">
-//             {allGrants.map((grantData, key) => {
+//             <div id= "grant-list">
+//             {grantList.map((grantData, key) => {
 //             return <GrantCard key={key} grantData={grantData} />;
 //             })}
 //             </div>
-
 //         </div>
 //     );
-    
 // }
-// export default GrantsPage;
+// export default GrantPage;
+
+// use the function below to test dummy data
+function GrantsPage() {
+    return (
+
+        <div>
+            <div id="h1-home">
+                <h1>Grants</h1>
+            </div>
+            <div id="grant-list">
+            {allGrants.map((grantData, key) => {
+            return <GrantCard key={key} grantData={grantData} />;
+            })}
+            </div>
+
+        </div>
+    );
+    
+}
+export default GrantsPage;
