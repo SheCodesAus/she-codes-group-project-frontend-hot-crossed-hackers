ae462f018a2b1adfa35a8ab7974e117bfd78bd1a
genesisscv <genesisscv@gmail.com>
2022-09-06 20:35:28 +1000

    deleted link

--- src/components/GrantDetails/GrantDetails.jsx
+++ src/components/GrantDetails/GrantDetails.jsx
@@ -6,15 +6,15 @@ function GrantDetails(props) {
     const { grantData } = props;
     return(
         <div className="details-container">
-        <Link to={`/grant/${grantData.id}`}>
-        <img src={grantData.image} alt="grant"/>
-        <div className="details-data">
-        <h1>{grantData.title}</h1>
-        <h2>Organisation: {grantData.organisation}</h2>
-        <h2>Description: {grantData.description}</h2>
-        <h2>More information at: {grantData.url}</h2>
-        </div>
-        </Link>
+            {/* <Link to={`/grant/${grantData.id}`}> */}
+                <img src={grantData.image} alt="grant"/>
+                <div className="details-data">
+                    <h1>{grantData.title}</h1>
+                    <h2>Organisation: {grantData.organisation}</h2>
+                    <h2>Description: {grantData.description}</h2>
+                    <h2>More information at: {grantData.url}</h2>
+                </div>
+            {/* </Link> */}
         </div>
     );
 }
