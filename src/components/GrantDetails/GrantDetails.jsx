0f8edac17ca81a7f736e047b4c5f87f79563a534
genesisscv <genesisscv@gmail.com>
2022-09-07 20:30:11 +1000

    Changed the name of some files since my page was picking up styles from a different page

--- src/components/GrantDetails/GrantDetails.jsx
+++ src/components/GrantDetails/GrantDetails.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./GrantDetails.css";
 
 function GrantDetails(props) {
     const { grantData } = props;
     return(
         <div className="details-container">
-        <img src={grantData.image} alt="grant"/>
-        <div className="details-data">
-            <h1>{grantData.title}</h1>
-            <h2>Organisation: {grantData.organisation}</h2>
-            <h2>Description: {grantData.description}</h2>
-            <h2>More information at: {grantData.url}</h2>
-        </div>
+            <img src={grantData.image} alt="grant"/>
+            <p id="main-title">{grantData.title}</p>
+            <p>Organisation: {grantData.organisation}</p>
+            <p>Description: {grantData.description}</p>
+            <p>More information at: {grantData.url}</p>  
         </div>
     );
 }
--- src/pages/GrantPage.jsx
+++ src/pages/GrantPage.jsx
@@ -1,45 +1,3 @@
-// import React from "react";
-// import { allGrants } from "../data";
-// import GrantDetails from "../components/GrantDetails/GrantDetails";
-// import { useParams} from 'react-router-dom'
-
-// function GrantPage() {
-//     const { id } = useParams();
-//     return (
-
-//         <div>
-//             <div>
-//                 <GrantDetails key="1" grantData={allGrants[id]} />
-        
-//             </div>
-
-//         </div>
-//     );
-    
-// }
-
-// export default GrantPage;
-
-// important {allGrants[id]} is going to be replaced with fetch request
-
-
-
-
-// import React from "react";
-// import "./GrantCard.jsx";
-// import { oneGrant } from "../data";
-
-
-// function GrantPage() {
-//     return (
-//     <div>
-//     <h2>{oneGrant.title}</h2>
-//     </div>
-//     );
-// }
-// export default GrantPage;
-
-// Function to get data from API below
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import GrantDetails from "../components/GrantDetails/GrantDetails";
@@ -63,3 +21,27 @@ function GrantPage() {
     )
 }
 export default GrantPage;
+
+
+// Function to test dummy data below
+// import React from "react";
+// import { allGrants } from "../data";
+// import GrantDetails from "../components/GrantDetails/GrantDetails";
+// import { useParams} from 'react-router-dom'
+
+// function GrantPage() {
+//     const { id } = useParams();
+//     return (
+
+//         <div>
+//             <div>
+//                 <GrantDetails key="1" grantData={allGrants[id]} />
+        
+//             </div>
+
+//         </div>
+//     );
+    
+// }
+
+// export default GrantPage;
