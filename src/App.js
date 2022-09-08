32388e58dec42a9b274efefc4ea970124a5635a9
genesisscv <genesisscv@gmail.com>
2022-09-08 13:42:45 +1000

    working on filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from
   "react-router-dom";
 import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage";
 import GrantsPage from "./pages/GrantsPage";
 import GrantPage from "./pages/GrantPage";
@@ -22,6 +23,7 @@ function App() {
         <Route path="signup" element={<SignUpPage />} />
         <Route path="account" element={<AccountPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
--- src/components/PartnerCard/PartnerCard.jsx
+++ src/components/PartnerCard/PartnerCard.jsx
@@ -5,7 +5,7 @@ function PartnerCard(props) {
     const { partnerData } = props;
     return(
         <div className="partner-card">
-            <img src={partnerData.image} alt="partner"/>
+            <a href={partnerData.url}><img src={partnerData.image} alt="partner"/></a>
         </div>
     );
 }
