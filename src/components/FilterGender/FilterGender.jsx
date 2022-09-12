8d5660dd73a9ea01fe182dbffe02e0ab39439d9d
genesisscv <genesisscv@gmail.com>
2022-09-12 15:12:53 +1000

    working on filters

---
+++ src/components/FilterGender/FilterGender.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./FilterGender.css";
+
+//Filter list by category in React JS
+export default function FilterGender(props) {
+  function handleCategoryChange(event) {
+    props.setSelectedCategory(event.target.value);
+  }
+
+  return (
+    <div className="app">
+      <div className="filter-container">
+        <div>Filter by: Gender</div>
+        <div>
+          <select
+            name="category-list"
+            id="category-list"
+            onChange={handleCategoryChange}
+          >
+            <option value="AN">Any</option>
+            <option value="FE">Female</option>
+            <option value="MA">Male</option>
+            <option value="NB">Non-Binary</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/GrantsPage.jsx
+++ src/pages/GrantsPage.jsx
@@ -2,30 +2,89 @@
 // import { allGrants } from "../data"; use this to test dummy data
 import GrantCard from "../components/GrantCard/GrantCard";
 import React, { useState, useEffect } from "react";
+import FilterGender from "../components/FilterGender/FilterGender";
+import FilterIndigenous from "../components/FilterIndigenous/FilterIndigenous";
+import FilterVision from "../components/FilterVision/FilterVision";
+import FilterIncome from "../components/FilterIncome/FilterIncome";
+import FilterEnglishSL from "../components/FilterEnglishSL/FilterEnglishSL";
+import FilterDuration from "../components/FilterDuration/FilterDuration";
 
 function GrantsPage() {
-    const [grantList, setGrantList] = useState([]);
-        useEffect(() => {
-            fetch(`${process.env.REACT_APP_API_URL}scholarships/`)
-            .then((results) => {
-                console.log(results);
-            return results.json();
-            })
-            .then((data) => {
-            setGrantList(data);
-            });
-            },[]);
+  const [grantList, setGrantList] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}scholarships/`)
+      .then((results) => {
+        console.log(results);
+        return results.json();
+      })
+      .then((data) => {
+        setGrantList(data);
+      });
+  }, []);
 
+  // Filter useEffect start
+  useEffect(() => {
+    const filteredGrantList = grantList.filter(
+      (grant) => grant.gender === selectedCategory
+    );
+    setGrantList(() => filteredGrantList);
+  }, [selectedCategory]);
 
-        return (
-            <div class="main-background">
-                <div id= "grant-list">
-                {grantList.map((grantData, key) => {
-                return <GrantCard key={key} grantData={grantData} />;
-                })}
-                </div>
-            </div>
-        );
+  useEffect(() => {
+    const filteredGrantList = grantList.filter(
+      (grant) => grant.indigenous_status === selectedCategory
+    );
+    setGrantList(() => filteredGrantList);
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    const filteredGrantList = grantList.filter(
+      (grant) => grant.vision_impairment === selectedCategory
+    );
+    setGrantList(() => filteredGrantList);
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    const filteredGrantList = grantList.filter(
+      (grant) => grant.low_income === selectedCategory
+    );
+    setGrantList(() => filteredGrantList);
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    const filteredGrantList = grantList.filter(
+      (grant) => grant.esol === selectedCategory
+    );
+    setGrantList(() => filteredGrantList);
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    const filteredGrantList = grantList.filter(
+      (grant) => grant.duration === selectedCategory
+    );
+    setGrantList(() => filteredGrantList);
+  }, [selectedCategory]);
+
+  // Filters useEffect end
+
+  return (
+    <div class="main-background">
+      <div id="filters">
+        <FilterGender setSelectedCategory={setSelectedCategory} />
+        <FilterIndigenous setSelectedCategory={setSelectedCategory} />
+        <FilterVision setSelectedCategory={setSelectedCategory} />
+        <FilterIncome setSelectedCategory={setSelectedCategory} />
+        <FilterEnglishSL setSelectedCategory={setSelectedCategory} />
+        <FilterDuration setSelectedCategory={setSelectedCategory} />
+      </div>
+      <div id="grant-list">
+        {grantList.map((grantData, key) => {
+          return <GrantCard key={key} grantData={grantData} />;
+        })}
+      </div>
+    </div>
+  );
 }
 export default GrantsPage;
 
@@ -45,6 +104,6 @@ export default GrantsPage;
 
 //         </div>
 //     );
-    
+
 // }
 // export default GrantsPage;
