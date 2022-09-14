024853c71011fc573305956200e0b0afd7a9140b
Jess van Dyk <jessica.vandyk@xplortechnologies.com>
2022-09-15 09:14:07 +1000

    Loading State
    added Spinner Component and Styling to match Nav Colours
    Fixed Class error. Was in as class instead of className

--- src/pages/GrantsPage.jsx
+++ src/pages/GrantsPage.jsx
@@ -8,11 +8,14 @@ import FilterVision from "../components/FilterVision/FilterVision";
 import FilterIncome from "../components/FilterIncome/FilterIncome";
 import FilterEnglishSL from "../components/FilterEnglishSL/FilterEnglishSL";
 import FilterDuration from "../components/FilterDuration/FilterDuration";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 function GrantsPage() {
+  const [loading, setLoading] = useState(false)
   const [grantList, setGrantList] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
   useEffect(() => {
+    setLoading(true)
     fetch(`${process.env.REACT_APP_API_URL}scholarships/`)
       .then((results) => {
         console.log(results);
@@ -20,6 +23,7 @@ function GrantsPage() {
       })
       .then((data) => {
         setGrantList(data);
+        setLoading(false)
       });
   }, []);
 
@@ -69,7 +73,8 @@ function GrantsPage() {
   // Filters useEffect end
 
   return (
-    <div class="main-background">
+    <div className="main-background">
+    {loading ? <LoadingSpinner /> : <div>
       <div id="filters">
         <FilterGender setSelectedCategory={setSelectedCategory} />
         <FilterIndigenous setSelectedCategory={setSelectedCategory} />
@@ -84,6 +89,7 @@ function GrantsPage() {
         })}
       </div>
     </div>
+  }</div>
   );
 }
 export default GrantsPage;
