ed862c985db6121b1fd37d2c775f25d4c93ee907
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-09-15 22:28:17 +1000

    styling

--- src/components/GrantCard/GrantCard.jsx
+++ src/components/GrantCard/GrantCard.jsx
@@ -8,7 +8,7 @@ function GrantCard(props) {
             <Link to={`/grant/${grantData.id}`}>  <img src={grantData.image} className='w-100 hover-shadow' alt="grant" /></Link>
             <h3 className="grant-title">{grantData.title}</h3>
             <h4 className="date"> Closing data: {new Date(grantData.closing_date).toDateString()}</h4>
-            <h4 className="description" >{grantData.description}</h4>
+            {/* <h4 className="description" >{grantData.description}</h4> */}
             <Link to={`/grant/${grantData.id}`}>
                 <h4 className="see-more-btn">
                     See more
