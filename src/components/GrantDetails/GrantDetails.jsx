46fc870c00f99cf3bf38a65d93f4a4f58015ad79
Sarah <sarahdavis398@gmail.com>
2022-09-16 23:53:08 +1000

    allow save button to each grant detail page

--- src/components/GrantDetails/GrantDetails.jsx
+++ src/components/GrantDetails/GrantDetails.jsx
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./GrantDetails.css";
 
 function GrantDetails(props) {
   const { grantData } = props;
+
+
+  const token = window.localStorage.getItem("token")
+  const navigate = useNavigate();
+
+  const username = window.localStorage.getItem("username")
+  const [userData, setUserData] = useState( {favorites: []});
+
+
+  useEffect(() => {
+      fetch(`${process.env.REACT_APP_API_URL}users/${username}`)
+          .then(res => res.json())
+          .then(data => { console.log(data) ;setUserData(data) })
+  }, [username])
+
+  const handleSave = (e) => {
+      {
+          fetch(
+              `${process.env.REACT_APP_API_URL}scholarships/favorites/${grantData.id}/`, {
+              method: "post",
+              headers: {
+                  'Authorization': `Token ${token}`,
+                  "Content-Type": "application/json"
+              }
+              
+          })
+              .then(res => {
+                  if (res.ok) { console.log("HTTP request successful"); navigate("/") }
+                  else { console.log("HTTP request unsuccessful") }
+              })
+      }
+  };
+
+  const handleDelete = (e) => {
+      {
+          fetch(
+              `${process.env.REACT_APP_API_URL}scholarships/favorites/${grantData.id}/`, {
+              method: "delete",
+              headers: {
+                  'Authorization': `Token ${token}`,
+                  "Content-Type": "application/json"
+              }
+          })
+              .then(res => {
+                  if (res.ok) { console.log("HTTP request successful"); navigate("/") }
+                  else { console.log("HTTP request unsuccessful") }
+              })
+      }
+  };
 
   
   return (
@@ -22,6 +72,21 @@ function GrantDetails(props) {
             <button onClick={handleDelete} className="save-btn" >UNSAVE</button> */}
         <br></br>
       </ul>
+
+      {window.localStorage.getItem('token') ?
+<div>
+        
+            {userData.favorites.includes(grantData.id) ? (
+        <button onClick={handleDelete} className="save-btn">
+        UNSAVE
+        </button>
+) : (
+        <button onClick={handleSave} className="save-btn">
+        SAVE
+        </button>
+)}
+</div>
+: null}
     </div>
   );
 }
