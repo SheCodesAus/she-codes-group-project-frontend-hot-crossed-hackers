66cc5c50c7a9e62bb750e7ee810cb723230554d0
Jess van Dyk <jessica.vandyk@xplortechnologies.com>
2022-09-16 23:21:00 +1000

    Button Link - Grants Page
    embedded link within Button

--- src/components/GrantDetails/GrantDetails.jsx
+++ src/components/GrantDetails/GrantDetails.jsx
@@ -7,7 +7,7 @@ function GrantDetails(props) {
   
   return (
     <div className="details-container">
-      <img src={grantData.image} alt="grant" />
+      <img id="single-pic-grant-page" src={grantData.image} alt="grant" />
       <p id="main-title">{grantData.title}</p>
       <ul>
         <li>Organisation: {grantData.organisation}</li> <br></br>
@@ -16,7 +16,7 @@ function GrantDetails(props) {
         <li>Eligibility: {grantData.eligibility}</li>
         <br></br>
         <a href={grantData.url}>
-          <li>More information at: {grantData.url}</li>
+          <button id="apply-button">Click Here To Find Out More!</button>
         </a>
         {/* <button onClick={handleSave} className="save-btn" >SAVE</button>
             <button onClick={handleDelete} className="save-btn" >UNSAVE</button> */}
