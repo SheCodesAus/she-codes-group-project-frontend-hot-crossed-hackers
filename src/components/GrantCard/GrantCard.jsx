8aa07245433b1adac4163d3fc679adabb4534132
Sarah <sarahdavis398@gmail.com>
2022-09-16 20:55:33 +1000

    enable save and unsave

--- src/components/GrantCard/GrantCard.jsx
+++ src/components/GrantCard/GrantCard.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./GrantCard.css";
+
 function GrantCard(props) {
     const { grantData } = props;
     const token = window.localStorage.getItem("token")
     const navigate = useNavigate();
+
+    const username = window.localStorage.getItem("username")
+    const [userData, setUserData] = useState("");
+    // const [savebutton, setSaveButton ] = useState(!!userData.favorites.includes(grantData.id))
+    // const [savebutton, setSaveButton ] = useState(false)
+
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}users/${username}`)
+            .then(res => res.json())
+            .then(data => { setUserData(data); console.log(data) })
+    }, [username])
 
     const handleSave = (e) => {
         {
@@ -41,23 +54,34 @@ function GrantCard(props) {
         }
     };
 
+    // if(grantData.favorites[0]) {
+    //     if(grantData.favorites.includes(grantData.id)){
+    //         setSaveButton(true)
+    //     }
+    // }
 
     return (
         <div className="grant-card">
             <Link to={`/grant/${grantData.id}`}>  <img src={grantData.image} className='w-100 hover-shadow' alt="grant" /></Link>
             <h3 className="grant-title">{grantData.title}</h3>
             <h4 className="date"> Closing data: {new Date(grantData.closing_date).toDateString()}</h4>
-            {/* <h4 className="description" >{grantData.description}</h4> */}
-            {/* <div className ="card-CTA"> */}
             <Link to={`/grant/${grantData.id}`}>
                 <h4 className="see-more-btn">
                     See more
                 </h4>
             </Link>
-            <button onClick={handleSave} className="save-btn" >SAVE</button>
+            {/* {savebutton ? 
             <button onClick={handleDelete} className="save-btn" >UNSAVE</button>
-            {/* </div> */}
+            : <button onClick={handleSave} className="save-btn" >SAVE</button>} */}
+
+            {/* {savebutton ? <button onClick={handleDelete} className="save-btn" >UNSAVE</button> :
+            <button onClick={handleSave} className="save-btn" >SAVE</button>} */}
+            <button onClick={handleDelete} className="save-btn" >UNSAVE</button>
+             <button onClick={handleSave} className="save-btn" >SAVE</button>
+
         </div >
+
+
     );
 }
 
