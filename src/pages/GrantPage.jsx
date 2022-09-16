645a655ff31a2b7508ccc698609a1001e69d0593
Jess van Dyk <jessica.vandyk@xplortechnologies.com>
2022-09-16 23:06:07 +1000

    add loading states to GrantPage
    includes catch for good measure!

--- src/pages/GrantPage.jsx
+++ src/pages/GrantPage.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import GrantDetails from "../components/GrantDetails/GrantDetails";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 function GrantPage() {
+    const [loading, setLoading] = useState(false);
     const { id } = useParams();
     const [grantData, setGrantData] = useState({})
     
     useEffect(() => {
+        setLoading(true);
         fetch(`${process.env.REACT_APP_API_URL}scholarships/${id}`)
             .then((results) => {
                 return results.json();
             })
             .then((data) => {
                 setGrantData(data);
+                setLoading(false);
+            })
+            .catch(() => {
+            setLoading(false);    
             });
     }, []);
 
     return (
-        <GrantDetails grantData={grantData}/>
+        <div id="single-grant">
+        {
+            loading 
+            ? <LoadingSpinner /> 
+            : <GrantDetails grantData={grantData}/>
+        }
+        </div>
     )
 }
 export default GrantPage;
